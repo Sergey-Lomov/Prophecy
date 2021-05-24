13ba2337cbcf9a2d250e61e4dfc5ded84e44fc41
serhii.lomov <serhii.lomov23@gmail.com>
2021-05-24 21:20:18 +0300

    Initial Commit

---
+++ Prophecy/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Prophecy
+//
+//  Created by Serhii.Lomov on 12.04.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var fateLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func getFate() {
+//        let name = nameField.text ?? ""
+//        let fateText = FateGenerator().fate(name: name)
+//        let fateEmoji = Emojinator().emojinate(string: fateText)
+//        fateLabel.text = fateEmoji
+    }
+}
+
